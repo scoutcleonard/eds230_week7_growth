cca084cd04ad50f015c816386ca8b3fc51764f5e
Brianna Baker <brirose.baker@gmail.com>
2022-05-11 09:27:03 -0700

    further structure setup

---
+++ R/growth_function.R
@@ -0,0 +1,16 @@
+
+
+#' Forest Growth Model
+#' Function to estimate forest growth
+#' 
+#'
+#' @param r early exponential growth rate
+#' @param g linear growth rate once canopy closure has been reached
+#' @param K Carrying capacity in units of carbon (C)
+#'
+#' @return Forest size in units of carbon (C)
+#' @export
+#'
+#' @examples
+#' 
+#' 
